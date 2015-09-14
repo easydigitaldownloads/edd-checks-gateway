c66c9354fe50d21a1c557f46352eed16b1593bc2
fxbenard <fxbenard@gmail.com>
2015-09-14 22:09:07 +0200

    Missing header + wpcs + missing textdomain

--- edd-checks-gateway.php
+++ edd-checks-gateway.php
@@ -7,13 +7,15 @@ Version: 1.3
 Author: Easy Digital Downloads Team
 Author URI: http://pippinsplugins.com
 Contributors: mordauk
+Text Domain: eddcg
+Domain Path: languages
 */
 
-if(!defined('EDDCG_PLUGIN_DIR')) {
-	define('EDDCG_PLUGIN_DIR', dirname(__FILE__));
+if ( ! defined( 'EDDCG_PLUGIN_DIR' ) ) {
+	define( 'EDDCG_PLUGIN_DIR', dirname( __FILE__ ) );
 }
 
-if( class_exists( 'EDD_License' ) && is_admin() ) {
+if ( class_exists( 'EDD_License' ) && is_admin() ) {
 	$edd_checks_license = new EDD_License( __FILE__, 'Check Payment Gateway', '1.3', 'Easy Digital Downloads', 'eddcg_license_key' );
 }
 
@@ -30,7 +32,6 @@ function edd_textdomain() {
 add_action( 'init', 'edd_textdomain' );
 
 
-
 /**
  * Register the payment gateway
  *
@@ -42,7 +43,7 @@ function eddcg_register_gateway($gateways) {
 	$gateways['checks'] = array( 'admin_label' => 'Checks', 'checkout_label' => __( 'Check', 'eddcg' ) );
 	return $gateways;
 }
-add_filter('edd_payment_gateways', 'eddcg_register_gateway');
+add_filter( 'edd_payment_gateways', 'eddcg_register_gateway' );
 
 
 /**
@@ -67,19 +68,19 @@ add_action( 'plugins_loaded', 'eddcg_disable_abandoned_orders' );
 function eddcg_payment_cc_form() {
 	global $edd_options;
 	ob_start(); ?>
-	<?php do_action('edd_before_check_info_fields'); ?>
-	<fieldset id="edd_check_payment_info">
+	<?php do_action( 'edd_before_check_info_fields' ); ?>
+    <fieldset id="edd_check_payment_info">
 		<?php
 		$settings_url = admin_url( 'edit.php?post_type=download&page=edd-settings&tab=gateways' );
-		$notes = ! empty( $edd_options['eddcg_checkout_notes'] ) ? $edd_options['eddcg_checkout_notes'] : sprintf( __('Please enter checkout instructions in the %s settings for paying by check.', 'eddcg'), '<a href="' . $settings_url . '">' . __('Payment Gateway', 'eddcg') . '</a>' );
+		$notes = ! empty( $edd_options['eddcg_checkout_notes'] ) ? $edd_options['eddcg_checkout_notes'] : sprintf( __( 'Please enter checkout instructions in the %s settings for paying by check.', 'eddcg' ), '<a href="' . $settings_url . '">' . __( 'Payment Gateway', 'eddcg' ) . '</a>' );
 		echo wpautop( stripslashes( $notes ) );
 		?>
-	</fieldset>
-	<?php do_action('edd_after_check_info_fields'); ?>
+    </fieldset>
+	<?php do_action( 'edd_after_check_info_fields' ); ?>
 	<?php
 	echo ob_get_clean();
 }
-add_action('edd_checks_cc_form', 'eddcg_payment_cc_form');
+add_action( 'edd_checks_cc_form', 'eddcg_payment_cc_form' );
 
 
 /**
@@ -92,7 +93,7 @@ function eddcg_process_payment($purchase_data) {
 
 	global $edd_options;
 
-	$purchase_summary = edd_get_purchase_summary($purchase_data);
+	$purchase_summary = edd_get_purchase_summary( $purchase_data );
 
 	// setup the payment details
 	$payment = array(
@@ -104,20 +105,20 @@ function eddcg_process_payment($purchase_data) {
 		'downloads' 	=> $purchase_data['downloads'],
 		'cart_details' 	=> $purchase_data['cart_details'],
 		'user_info' 	=> $purchase_data['user_info'],
-		'status' 		=> 'pending'
+		'status' 		=> 'pending',
 	);
 
 	// record the pending payment
-	$payment = edd_insert_payment($payment);
+	$payment = edd_insert_payment( $payment );
 
-	if( $payment ) {
+	if ( $payment ) {
 		edd_cg_send_admin_notice( $payment );
 		eddcg_send_payment_instructions_email( $payment );
 		edd_empty_cart();
 		edd_send_to_success_page();
 	} else {
 		// if errors are present, send the user back to the purchase page so they can be corrected
-		edd_send_back_to_checkout('?payment-mode=' . $purchase_data['post_data']['edd-gateway']);
+		edd_send_back_to_checkout( '?payment-mode=' . $purchase_data['post_data']['edd-gateway'] );
 	}
 
 }
@@ -137,7 +138,7 @@ function edd_cg_send_admin_notice( $payment_id = 0 ) {
 	$user_info   = edd_get_payment_meta_user_info( $payment_id );
 
 	if ( isset( $user_info['id'] ) && $user_info['id'] > 0 ) {
-		$user_data = get_userdata($user_info['id']);
+		$user_data = get_userdata( $user_info['id'] );
 		$name      = $user_data->display_name;
 	} elseif ( isset( $user_info['first_name'] ) && isset($user_info['last_name'] ) ) {
 		$name = $user_info['first_name'] . ' ' . $user_info['last_name'];
@@ -150,7 +151,7 @@ function edd_cg_send_admin_notice( $payment_id = 0 ) {
 	$admin_subject = apply_filters( 'eddcg_admin_purchase_notification_subject', __( 'New pending purchase', 'eddcg' ), $payment_id );
 
 	$admin_message = __( 'Hello', 'eddcg' ) . "\n\n" . sprintf( __( 'A %s purchase has been made', 'eddcg' ), edd_get_label_plural() ) . ".\n\n";
-	$admin_message.= sprintf( __( '%s sold:', 'eddcg' ), edd_get_label_plural() ) .  "\n\n";
+	$admin_message .= sprintf( __( '%s sold:', 'eddcg' ), edd_get_label_plural() ) .  "\n\n";
 
 	$download_list = '';
 	$downloads     = edd_get_payment_meta_downloads( $payment_id );
@@ -169,8 +170,8 @@ function edd_cg_send_admin_notice( $payment_id = 0 ) {
 
 	$order_url      = admin_url( 'edit.php?post_type=download&page=edd-payment-history&edd-action=view-order-details&id=' . $payment_id );
 	$admin_message .= $download_list . "\n";
-	$admin_message .= __( 'Purchased by: ', 'eddcg' )   . " " . html_entity_decode( $name, ENT_COMPAT, 'UTF-8' ) . "\n";
-	$admin_message .= __( 'Amount: ', 'eddcg' )         . " " . html_entity_decode( $amount, ENT_COMPAT, 'UTF-8' ) . "\n\n";
+	$admin_message .= __( 'Purchased by: ', 'eddcg' )   . ' ' . html_entity_decode( $name, ENT_COMPAT, 'UTF-8' ) . "\n";
+	$admin_message .= __( 'Amount: ', 'eddcg' )         . ' ' . html_entity_decode( $amount, ENT_COMPAT, 'UTF-8' ) . "\n\n";
 	$admin_message .= __( 'This is a pending purchase awaiting payment.', 'eddcg' ) . "\n\n";
 	$admin_message .= sprintf( __( 'View Order Details: %s.', 'eddcg' ), $order_url ) . "\n\n";
 	$admin_message  = apply_filters( 'eddcg_admin_purchase_notification', $admin_message, $payment_id );
@@ -218,16 +219,16 @@ function eddcg_add_settings($settings) {
 	$check_settings = array(
 		array(
 			'id'      => 'check_payment_settings',
-			'name'    => '<strong>' . __('Check Payment Settings', 'eddcg') . '</strong>',
-			'desc'    => __('Configure the Check Payment settings', 'eddcg'),
-			'type'    => 'header'
+			'name'    => '<strong>' . __( 'Check Payment Settings', 'eddcg' ) . '</strong>',
+			'desc'    => __( 'Configure the Check Payment settings', 'eddcg' ),
+			'type'    => 'header',
 		),
 		array(
 			'id'      => 'eddcg_checkout_notes',
-			'name'    => __('Check Payment Instructions', 'eddcg'),
+			'name'    => __( 'Check Payment Instructions', 'eddcg' ),
 			'desc'    => sprintf( __( 'Enter the instructions you want to show to the buyer during the checkout process here. This should probably include your mailing address and who to make the check out to. Configure email settings <a href="%s">here</a>.', 'eddcg' ), esc_url( admin_url( '/edit.php?post_type=download&page=edd-settings&tab=emails' ) ) ),
-			'type'    => 'rich_editor'
-		)
+			'type'    => 'rich_editor',
+		),
 	);
 
 	return array_merge( $settings, $check_settings );
@@ -247,26 +248,26 @@ function eddcg_add_email_settings( $settings ) {
 			'id'      => 'check_payment_email_settings',
 			'name'    => '<strong>' . __( 'Check Payment Notification', 'eddcg' ) . '</strong>',
 			'desc'    => __( 'Configure the Check Payment settings', 'eddcg' ),
-			'type'    => 'header'
+			'type'    => 'header',
 		),
 		array(
 			'id'      => 'eddcg_pending_email_subject',
 			'name'    => __( 'Payment Instructions Email Subject', 'eddcg' ),
 			'desc'    => __( 'The subject line for the Payment Instructions Email.', 'eddcg' ),
-			'type'    => 'text'
+			'type'    => 'text',
 		),
 		array(
 			'id'      => 'eddcg_pending_email_heading',
 			'name'    => __( 'Payment Instructions Email Heading', 'eddcg' ),
 			'desc'    => __( 'The heading for the Payment Instructions Email body.', 'eddcg' ),
-			'type'    => 'text'
+			'type'    => 'text',
 		),
 		array(
 			'id'      => 'eddcg_pending_email',
 			'name'    => __( 'Payment Instructions Email Body', 'eddcg' ),
-			'desc'    => sprintf( __( 'Enter the instructions you want to email to the buyer after the checkout process. This should probably include your mailing address and who to make the check out to. Available template tags: <br> %s ' ), edd_get_emails_tags_list() ),
-			'type'    => 'rich_editor'
-		)
+			'desc'    => sprintf( __( 'Enter the instructions you want to email to the buyer after the checkout process. This should probably include your mailing address and who to make the check out to. Available template tags: <br> %s ', 'eddcg' ), edd_get_emails_tags_list() ),
+			'type'    => 'rich_editor',
+		),
 	);
 	return array_merge( $settings, $email_settings );
 }
